7fc21708ad4a84fcef60865692376c6ae0dd9d75
Anglo Essam Saber <eng.angloesam2010@gmail.com>
2018-01-08 14:38:29 +0300

    Initial Commit

---
+++ Game_pokemon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game_pokemon
+//
+//  Created by Eng Anglo Essam Saber on 1/8/18.
+//  Copyright © 2018 Eng Anglo Essam Saber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
